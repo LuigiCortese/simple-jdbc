dcc3e72a8968486982d4d36f20aba9afccf3a1db
Luigi Cortese <luigi.cortese00@gmail.com>
2015-03-19 16:52:30 +1100

    Added query and populate table

--- src/com/mui/jdbcproject/Main.java
+++ src/com/mui/jdbcproject/Main.java
@@ -2,15 +2,15 @@ package com.mui.jdbcproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
-
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Main {
 
-	String userName = "**";
-	String password = "**";
+	String userName = "utentedrupal";
+	String password = "passworddrupal";
 	String dbms = "mysql";
 	String serverName = "localhost";
 	String portNumber = "3306";
@@ -23,13 +23,14 @@ public class Main {
 
 	void operate() {
 		try {
-			//create connection
+			
 			connection = connect();
 			
-			//execute DDL
 			createTable();
 			
-			//execute Query
+			populateTable();
+			
+			viewTable();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -60,7 +61,7 @@ public class Main {
 	}
 
 	/**
-	 * Create a table
+	 * Create table SUPPLIERS
 	 * 
 	 * @throws SQLException
 	 */
@@ -76,6 +77,60 @@ public class Main {
 		try {
 			stmt = connection.createStatement();
 			stmt.executeUpdate(createString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+	}
+	
+	/**
+	 * Populate table SUPPLIERS
+	 * 
+	 * @throws SQLException
+	 */
+	public void populateTable() throws SQLException {
+
+	    Statement stmt = null;
+	    try {
+	        stmt = connection.createStatement();
+	        stmt.executeUpdate(
+	        	"insert into " + dbName +".SUPPLIERS values(49, 'Superior Coffee', '1 Party Place', 'Mendocino', 'CA', '95460')");
+	        stmt.executeUpdate(
+	            "insert into " + dbName +".SUPPLIERS values(101, 'Acme, Inc.', '99 Market Street', 'Groundsville', 'CA', '95199')");
+	        stmt.executeUpdate(
+	            "insert into " + dbName +".SUPPLIERS values(150, 'The High Ground', '100 Coffee Lane', 'Meadows', 'CA', '93966')");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (stmt != null) { stmt.close(); }
+	    }
+	}
+	
+	/**
+	 * Query table SUPPLIERS
+	 * 
+	 * @param con
+	 * @throws SQLException
+	 */
+	public void viewTable() throws SQLException {
+		Statement stmt = null;
+		String query = "select SUP_ID, SUP_NAME, STREET, CITY, STATE, ZIP from SUPPLIERS";
+		try {
+			stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				int supplierID = rs.getInt("SUP_ID");
+				String supplierName = rs.getString("SUP_NAME");
+				String street = rs.getString("STREET");
+				String city = rs.getString("CITY");
+				String state = rs.getString("STATE");
+				String zip = rs.getString("ZIP");
+				System.out.println(supplierName + "(" + supplierID + "): "
+						+ street + ", " + city + ", " + state + ", " + zip);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
